99cbdc89a85604765d42650106937888d3295d4f
Cauan Moronhe <cauanmoronhelopes@gmail.com>
2026-05-29 12:13:05 -0300

    Created GET and POST methods in the project

---
+++ src/routes/game.routes.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { createNewGame, findGames } from "../controllers/game.controller";
+
+
+const router = Router();
+
+router.get("/games", findGames);
+
+router.post("/games", createNewGame);
+
+export default router;
